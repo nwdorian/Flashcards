39798f83c8177dbf100a558e3e4e839d220c501f
nwdorian <neuwirthdorian@gmail.com>
2024-03-04 17:01:19 +0100

    add AppConfig class

---
+++ FlashcardsLibrary/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FlashcardsLibrary;
+internal class AppConfig
+{
+    private static IConfiguration _iconfiguration;
+
+    static AppConfig()
+    {
+        GetAppSettingsFile();
+    }
+
+    public static void GetAppSettingsFile()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json");
+            
+        _iconfiguration = builder.Build();
+    }
+
+    public static string GetConnectionString()
+    {
+        return _iconfiguration.GetConnectionString("Default");
+    }
+}
